2306099ae76585fbe7b206b17f7ec2a144385da0
Camilo Oviedo <camilo.oviedo@readinessit.com>
2019-06-27 16:08:13 -0400

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
+
+//Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+// Body Parser 
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
 
 // Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -14,19 +30,12 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 });
 
 //Rutas
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-
-
-    });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 // Escuchar peticiones
 app.listen(3000, () => {
     console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
 
-})
+});
